abd30a90bda4f6362bac709f1afb062a304d66dc
IltonBJSilva <xlsonplay@gmail.com>
2019-04-03 17:41:05 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/sankhya/olaweb/PrimeiroServlet/App.java
@@ -0,0 +1,13 @@
+package br.com.sankhya.olaweb.PrimeiroServlet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
